d510a7f947e840730d35fe919016ee91b22dafbf
armaanas <armaanrsalam@gmail.com>
2023-05-30 20:37:45 +0100

    Add new blog: generative-ai

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,23 +3,27 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[slug].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/world.tsx";
+import * as $0 from "./routes/[blog].tsx";
+import * as $1 from "./routes/_404.tsx";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/world.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/Date.tsx";
+import * as $$2 from "./islands/Introduction.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[slug].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
-    "./routes/world.tsx": $3,
+    "./routes/[blog].tsx": $0,
+    "./routes/_404.tsx": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/index.tsx": $3,
+    "./routes/world.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
     "./islands/Date.tsx": $$1,
+    "./islands/Introduction.tsx": $$2,
   },
   baseUrl: import.meta.url,
   config,
