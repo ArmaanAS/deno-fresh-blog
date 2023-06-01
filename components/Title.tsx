aef41698a67659645fc57da733c9547a25a68e8e
armaanas <armaanrsalam@gmail.com>
2023-06-01 16:50:53 +0100

    Improve Title stylings

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,18 +1,18 @@
 import { tw } from "twind";
 
-const roundStyle = `p-4 rounded-full border overflow-hidden shadow-2xl my-8 border-t-0 border-b-0`;
-const clearStyle = `mt-4`;
+const roundStyle = `rounded-full border-2 border-yellow-200 shadow-md`;
+const clearStyle = `mt-4x`;
 
 export default function Title({ clear = false }: { clear?: boolean; }) {
   return (
     <a href="/"
-      class={tw(clear ? clearStyle : roundStyle)}>
+      class={tw(clear ? clearStyle : roundStyle) + " p-4 my-12 flex gap-4 items-center"}>
       <img
         src="/logo.svg"
-        class="w-36 h-36 mx-auto"
+        class="w-32 h-32 m-auto"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
-      <h1 class="text-6xl font-mono font-bold tracking-tighter text-center text-yellow-600 mb-16">this.blog</h1>
+      <h1 class="text-6xl font-mono font-bold tracking-tighter text-center text-yellow-400">this.blog</h1>
     </a>
   );
 }
