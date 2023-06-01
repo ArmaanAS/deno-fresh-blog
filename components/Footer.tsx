47041d4401ceafe161ae67aeee2d51289a5049ea
armaanas <armaanrsalam@gmail.com>
2023-06-01 11:21:57 +0100

    Add link to home in Copyright text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer class="mt-8 mb-24 text-center font-semibold">
-      <p>Copyright © 2023 this.blog</p>
+      <p>Copyright © 2023 <a href="/">this.blog</a></p>
     </footer>
   );
 }
