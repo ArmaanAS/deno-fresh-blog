5349d6ef952fb2ce7cfa3d9c751501e866345133
armaanas <armaanrsalam@gmail.com>
2023-06-15 12:39:21 +0100

    Add yellow tint to whole site

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,12 +4,23 @@ import Posts from "./Posts.tsx";
 import Title from "./Title.tsx";
 import { Head } from "$fresh/runtime.ts";
 import { Post, getNewPostsWithoutViews } from "../posts.ts";
+import { css, tw } from "twind/css";
 
 interface Props extends ComponentProps<"main"> {
   clearTitle?: boolean;
   pageTitle?: string;
   posts?: Post[];
 }
+
+const global = css({
+  ":global": {
+    html: {
+      filter: "contrast(0.8) brightness(1.2)",
+      mixBlendMode: "multiply",
+      backgroundColor: "#fffcea"
+    }
+  }
+});
 
 export default function Layout({
   clearTitle, pageTitle, children, class: className = "", posts, ...props
@@ -20,13 +31,17 @@ export default function Layout({
   return <>
     <Head>
       <title>{title}</title>
+      <meta name="description" content="A blog by AI." />
+      <meta name="author" content="AI" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#fef3c7" />
     </Head>
 
     <header class="flex justify-center">
       <Title clear={clearTitle} />
     </header>
 
-    <main class={"flex flex-col justify-center mx-auto p-6 max-w-screen-md " + className} {...props}>
+    <main class={"flex flex-col justify-center mx-auto p-6 max-w-screen-md " + className + tw(global)} {...props}>
       {children}
 
       <Posts posts={posts ?? getNewPostsWithoutViews()} />
--- components/Title.tsx
+++ components/Title.tsx
@@ -1,6 +1,6 @@
 import { tw } from "twind";
 
-const roundStyle = `my-12 rounded-full border-2 border-yellow-200 shadow-md`;
+const roundStyle = `my-12 rounded-full border-2 border-yellow-400 shadow-xl`;
 const clearStyle = `mt-8`;
 
 export default function Title({ clear = false }: { clear?: boolean; }) {
@@ -9,10 +9,10 @@ export default function Title({ clear = false }: { clear?: boolean; }) {
       class={tw(clear ? clearStyle : roundStyle) + " py-4 px-8 flex flex-wrap justify-center gap-4 items-center max-w-screen-md"}>
       <img
         src="/logo.svg"
-        class="w-20 h-20 sm:(w-24 h-24) md:(w-28 h-28) lg:(w-36 h-36) m-auto"
+        class="w-20 h-20 sm:(w-24 h-24) md:(w-28 h-28) lg:(w-36 h-36) m-auto brightness-50"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
-      <h1 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-mono font-bold tracking-tighter text-center text-yellow-400">this.blog</h1>
+      <h1 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-mono font-bold tracking-tighter text-center text-yellow-500">this.blog</h1>
     </a>
   );
 }
